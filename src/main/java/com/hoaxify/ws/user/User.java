3b4dcf1b891d4c1e491f67abf81d6d7853d49d64
İlker  Yelekçioğlu <ilker.yelekcioglu@eveshop.com.tr>
2024-05-01 22:53:10 +0300

    User parameter added and new UniqueConstraints files created

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,21 +1,34 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.user.validation.UniqueEmail;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "USERS",uniqueConstraints = @UniqueConstraint(columnNames = {"email"}))
 public class User {
 
     @Id
     @GeneratedValue
     private long id;
+
+    @Size(min = 4,max = 255)
+    @NotBlank
     private String username;
 
+    @NotBlank
+    @Email
+    @UniqueEmail
     private String email;
 
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d).*$")
     private String password;
 
     public String getUsername() {
