07ca0812593afbe706194c18694ee2c629a8af89
İlker  Yelekçioğlu <ilker.yelekcioglu@eveshop.com.tr>
2024-05-18 13:19:22 +0300

    user-page first page is ended

--- src/main/java/com/hoaxify/ws/shared/Messages.java
+++ src/main/java/com/hoaxify/ws/shared/Messages.java
@@ -1,11 +1,18 @@
 package com.hoaxify.ws.shared;
 
+import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Messages {
 
   public static String getMessageForLocale(String messageKey, Locale locale){
+    return ResourceBundle.getBundle("messages",locale).getString(messageKey);
+  }
+
+  public static String getMessageForLocale(String messageKey, Locale locale,Object... arguments){
+    String message = getMessageForLocale(messageKey, locale);
+    MessageFormat.format(message,arguments);
     return ResourceBundle.getBundle("messages",locale).getString(messageKey);
   }
 
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -7,7 +7,11 @@ import com.hoaxify.ws.user.dto.UserCreate;
 import com.hoaxify.ws.user.dto.UserDTO;
 import com.hoaxify.ws.user.exception.ActivationNotificationException;
 import com.hoaxify.ws.user.exception.InvalidTokenException;
+import com.hoaxify.ws.user.exception.NotFoundException;
 import com.hoaxify.ws.user.exception.NotUniqueEmailException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +52,11 @@ public class UserController {
   Page<UserDTO> getUsers(Pageable pageable){
     return  userService.getUsers(pageable).map(UserDTO::new);
   }
+@GetMapping("/api/v1/users/{id}")
+UserDTO getUserById(@PathVariable long id) {
+    return new UserDTO(userService.getUser(id));
+}
+
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   ResponseEntity<ApiError> handleMethodArgNotValidEx(MethodArgumentNotValidException exception) {
@@ -83,11 +92,21 @@ public class UserController {
     return ResponseEntity.status(502).body(apiError);
   }
   @ExceptionHandler(InvalidTokenException.class)
-  ResponseEntity<ApiError>  handleInvalidTokenException(InvalidTokenException exception) {
+  ResponseEntity<ApiError>  handleInvalidTokenException(InvalidTokenException exception,
+      HttpServletRequest request) {
     ApiError apiError = new ApiError();
-    apiError.setPath("/api/v1/users");
+    apiError.setPath(request.getRequestURI());
     apiError.setMessage(exception.getMessage());
     apiError.setStatus(400);
     return ResponseEntity.status(400).body(apiError);
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  ResponseEntity<ApiError>  handleNotFoundException(NotFoundException exception,HttpServletRequest request) {
+    ApiError apiError = new ApiError();
+    apiError.setPath(request.getRequestURI());
+    apiError.setMessage(exception.getMessage());
+    apiError.setStatus(404);
+    return ResponseEntity.status(404).body(apiError);
   }
 }
